2f215b26f4cfbe2e9b13d64eb348489291976743
ELZA <elzahanindyantika@gmail.com>
2023-05-11 16:38:45 +0700

    uts pbo

--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,20 @@
-package pengguna;
+package mhs;
 
-class Mhs {
-    Mhs () {
+public class Mhs{
+    String Nama;
+    String Nim;
+    int Smt;
 
+    void setNama(String Nama){
+        this.nama = nama;
     }
+
+    void setNim(String Nim){
+        this.nim = nim;
+    }
+
+    void setSmt(String Smt){
+        this.smt = smt;
+    }
+
 }
--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,15 @@
 package pengguna;
 
 public class User {
+    String Username;
+    String Password;
+
+    void set.Username(String Username){
+        this.Username = Username;
+    }
+
+    void set.Password(String Password){
+        this.Password = Password;
+    }
 
 }
